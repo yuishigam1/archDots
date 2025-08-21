cb59dbf619f98ba7a25303051e9fbd569f72d603
Matariya Yash <xofficialyashx@gmail.com>
2025-08-21 11:46:24 +0530

    fastCommit

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -12,7 +12,7 @@ safe_sync() {
 
 echo ">>> Deploying icon theme"
 mkdir -p "$HOME/.icons"
-safe_sync "$DOTFILES_DIR/.icons/Papirus-Everblush" "$HOME/.icons/Papirus-Everblush"
+safe_sync "$DOTFILES_DIR/.icons/Papirus-Everblush" "$HOME/.icons"
 
 echo ">>> Deploying .config"
 mkdir -p "$HOME/.config"
