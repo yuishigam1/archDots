1a87e8ef22507614ca556e0aa75f4ac9a401a3c0
Matariya Yash <xofficialyashx@gmail.com>
2025-08-21 14:18:37 +0530

    fastCommit

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -4,12 +4,14 @@ set -euo pipefail
 # Absolute path to the repo root
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 DOTFILES_DIR="$(realpath "$SCRIPT_DIR/../..")" # adjust depending on script location
-
+echo $SCRIPT_DIR
+echo $DOTFILES_DIR
 # Directories to sync
 for dir_name in myApps myIcons; do
   SRC="$DOTFILES_DIR/$dir_name"
   DEST="$HOME/$dir_name"
-
+  echo $SRC
+  echo $DEST
   if [ -d "$SRC" ]; then
     mkdir -p "$DEST"
     rsync -a --delete "$SRC/" "$DEST/"
